a54f125779465bb4af877ef48ac8ccb1bf7bb453
sanikab90 <54967929+sanikab90@users.noreply.github.com>
2019-11-17 13:05:43 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 /*
  * Sanika Buche
- * ssb170002
  * 
  * A program that takes a file of names and characters and outputs a file
  * of all the names baseball stats including the leaders in each category
